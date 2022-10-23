ddff5875019d0e79b854c8fda2c2c4b070fe64d2
KrisNathan <kristophernathanael2005@gmail.com>
2022-10-23 18:28:51 +0700

    added call rust add from cpp

--- call_rust.cpp
+++ call_rust.cpp
@@ -1,5 +1,7 @@
 #include "my_header.h"
+#include <iostream>
 int main() {
 	hello_from_rust();
+	std::cout << add(-2, 2) << '\n';
 	return 0;
 }
--- my_header.h
+++ my_header.h
@@ -6,6 +6,8 @@
 
 extern "C" {
 
+intptr_t add(intptr_t left, intptr_t right);
+
 void hello_from_rust();
 
 } // extern "C"
